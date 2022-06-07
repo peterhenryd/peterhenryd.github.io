20a7d994858883eb7ba6b4e65f85fbf437afdbd0
Peter <peter@Christines-MacBook-Pro.local>
2022-06-07 13:24:16 -0400

    Adding github pages deploy workflow

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- main nav -->
+  <div>
+
+  </div>
+
+  <!-- margin info section -->
+  <div></div>
+
+  <!-- projects -->
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
